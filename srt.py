137f99e31968478fae1bc2628707981a2842e9d5
Sanidhya27 <cool.sanidhyagwl@gmail.com>
2017-06-18 01:26:54 +0530

    final working

--- srt.py
+++ srt.py
@@ -1,44 +1,38 @@
 import os,time,re
 path='./The-Chainsmokers-and-Coldplay-Something-Just-Like-This-(Lyric).srt'
-#path='Eminem-Rap-God-(Explicit).srt'
+path='Eminem-Rap-God-(Explicit).srt'
 srt_file=open(path,'r')
 lines=srt_file.readlines()
 i=0
 timeregex=re.compile(r'(\d\d):(\d\d):(\d\d),(\d\d\d) --> (\d\d):(\d\d):(\d\d),(\d\d\d)')
-	
-inn=timeregex.search(lines[1])
-print(int(inn.group(1))*60*60+int(inn.group(2))*60+int(inn.group(3))+int(inn.group(4))*0.001)
-time.sleep(int(inn.group(1))*60*60+int(inn.group(2))*60+int(inn.group(3))+int(inn.group(4))*0.001)
+f=timeregex.search(lines[0])
 t=0
+str=""
 s=0
 k=0
-##print(lines[0:10])
-while(True):
-	if(i>=len(lines)):
-		break
-	lines[i]=lines[i][0:lines[i].find("\r\n")]
-	f=timeregex.search(lines[i])
-	#inn=timeregex.search(lines[i+1])
 
+while(i<len(lines)):
+	lines[i]=lines[i][0:lines[i].find("\n")]
+	f=timeregex.search(lines[i])
 	if(not(f==None)):
+		
 		t=(int(f.group(5))-int(f.group(1)))*60*60+(int(f.group(6))-int(f.group(2)))*60+(int(f.group(7))-int(f.group(3)))+(int(f.group(8))-int(f.group(4)))*0.001	
 		time_end_of_i=int(f.group(5))*60*60+int(f.group(6))*60+int(f.group(7))+int(f.group(8))*0.001
 		time_start_of_i=int(f.group(1))*60*60+int(f.group(2))*60+int(f.group(3))+int(f.group(4))*0.001
 		time.sleep(time_start_of_i-k)
 		k=time_end_of_i
-
-		
-		
+				
 	if(not((lines[i]).isnumeric()) and ((f==None) and lines[i]is not "" )):
-		print(lines[i],t)
-		s=s+t
+		str+=lines[i]+" "
 		
-		time.sleep(t)
+	if(lines[i]==""):
 		os.system('clear')
-		t=0
-	i+=1
-
+		print(str)	
+		time.sleep(t)
+		str=""
+			
+	i=i+1
+	
 	
 
-print(s)
 
